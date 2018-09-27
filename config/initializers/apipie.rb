d80a6c2e273a1f109b9c8eab69a04250cbf98b7f
Jorge T <jotolo7@gmail.com>
2018-09-27 17:00:09 +0200

    Fixing Apipie initializer default route

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,6 +1,6 @@
 Apipie.configure do |config|
   config.app_name                = 'BadiCodingChallengeBackend'
-  config.api_base_url            = '/api'
+  config.api_base_url            = ''
   config.doc_base_url            = '/documentation'
   config.validate                = false
   config.translate               = false
