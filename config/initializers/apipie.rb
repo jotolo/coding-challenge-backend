2b7d5fa34933fea92c9af2fd80267225461fb796
Jorge T <jotolo7@gmail.com>
2018-09-27 16:58:42 +0200

    Adding Static Doc

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -4,7 +4,7 @@ Apipie.configure do |config|
   config.doc_base_url            = '/documentation'
   config.validate                = false
   config.translate               = false
-  config.default_locale          = nil
+  config.default_locale          = :en
   config.app_info['1.0'] = "API documentation for Badi's Coding Challenge Backend."
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
